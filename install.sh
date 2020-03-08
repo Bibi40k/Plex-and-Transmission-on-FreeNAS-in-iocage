9d73cd2d9797be9adbe7e5d2ad3703d9756925c6
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-08 06:24:40 +0200

    z

--- install.sh
+++ install.sh
@@ -2,12 +2,16 @@
 # Build Plex & Transmission Iocage Jail under FreeNAS
 
 
-# Getting installer dir ( /mnt/${POOL_NAME}/BackUPS/Jails/Plex-and-Transmission-on-FreeNAS-in-Iocage )
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
+# Getting installer dir ( /root/Plex-and-Transmission-on-FreeNAS-in-Iocage )
+CWD="`pwd`"
+SCRIPT="`which $0`"
+RELDIR="`dirname $SCRIPT`"
+cd "$RELDIR"
+DIR="`pwd`"
+cd "$CWD"
 
 echo "SCRIPTPATH is $SCRIPTPATH"
 
 
 # Import scripts from /scripts dir
-source $SCRIPTPATH/scripts/dirs.sh # create all dir structure
+source ${DIR}/scripts/dirs.sh # create all dir structure
