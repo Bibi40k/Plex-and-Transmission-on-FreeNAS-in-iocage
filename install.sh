c98081c61b14bc543a928a6188eb251180c70854
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-08 06:47:20 +0200

    z

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@
 # Getting installer dir ( /root/Plex-and-Transmission-on-FreeNAS-in-Iocage )
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "${SCRIPT}")
-echo "SCRIPTPATH is $SCRIPTPATH"
-echo "${SCRIPTPATH}/scripts/dirs.sh"
 
 
 # Import scripts from /scripts dir
--- scripts/dirs.sh
+++ scripts/dirs.sh
@@ -3,7 +3,6 @@
 
 # Setting workdir and backup dir
 POOL_NAME=$(zpool list | grep mnt | awk '{print $1;}')
-echo "POOL_NAME is $POOL_NAME"
 
 # Default BackUp dir
 DBKP="/mnt/${POOL_NAME}/BackUP/Jails"
@@ -14,15 +13,16 @@ if [ ! -z $CUSTOM_DBKP ]; then
   read -p "We already set BackUp dir as [${CUSTOM_DBKP}]: " TMP_CUSTOM_DBKP
   TMP_CUSTOM_DBKP=${TMP_CUSTOM_DBKP:-$CUSTOM_DBKP}
 else
-  read -p "BackUp dir not set, default BackUp dir is [${DBKP}]: " TMP_DBKP
-  TMP_DBKP=${TMP_DBKP:-$DBKP}
+  read -p "BackUp dir not set, default BackUp dir is [${DBKP}]: " TMP_CUSTOM_DBKP
+  TMP_CUSTOM_DBKP=${TMP_CUSTOM_DBKP:-$DBKP}
 fi
 
-exit 1;
-
-
-DIRS=(../${SCRIPTPATH}/plex-configs,../${SCRIPTPATH}/transmission-configs)
+# Create dirs, copy dummy vars file and set bkp dir
+DIRS=(../${TMP_CUSTOM_DBKP}/plex-configs,../${TMP_CUSTOM_DBKP}/transmission-configs)
 mkdir -p -- "${DIRS[@]}"
+\cp -n "${SCRIPTPATH}/src/dummy-jail.vars" "${TMP_CUSTOM_DBKP}/jail.vars"
+sed -i "" "s/CUSTOM_BKP_DIR=.*/CUSTOM_BKP_DIR=\"${TMP_CUSTOM_DBKP}\"/" ${FVARS}
+
 
 exit 1;
 
