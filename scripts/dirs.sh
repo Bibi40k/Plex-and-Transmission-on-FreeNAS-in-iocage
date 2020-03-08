15bd9fc8eeb9f4a39db3ea9a56421e7150d5f7d9
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-08 22:39:41 +0200

    z

--- scripts/dirs.sh
+++ scripts/dirs.sh
@@ -2,11 +2,11 @@
 # This script will create all necessary dirs & store them in vars
 
 
-mkdir -p ${TMP_CUSTOM_DBKP}/plex-configsss
-ls -la ${TMP_CUSTOM_DBKP}
+mkdir -p ${TMP_CUSTOM_BKP_DIR}/plex-configsss
+ls -la ${TMP_CUSTOM_BKP_DIR}
 
 # Create dirs, copy dummy vars file and set bkp dir
-DIRS=(${TMP_CUSTOM_DBKP}/plex-configs,${TMP_CUSTOM_DBKP}/transmission-configs)
+DIRS=(${TMP_CUSTOM_BKP_DIR}/plex-configs,${TMP_CUSTOM_BKP_DIR}/transmission-configs)
 mkdir -p -- "${DIRS[@]}"
 
 
