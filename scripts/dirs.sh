dd0ff9c1442f27fdd1ebb566ce73faf85569deff
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-08 14:04:13 +0200

    z

--- scripts/dirs.sh
+++ scripts/dirs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # This script will create all necessary dirs & store them in vars
 
+
 # Setting workdir and backup dir
 POOL_NAME=$(zpool list | grep mnt | awk '{print $1;}')
 # Default BackUp dir
 DBKP="/mnt/${POOL_NAME}/BackUP/Jails"
+
 
 # $CUSTOM_BACKUP_DIR
 echo ""
@@ -16,11 +18,12 @@ else
   TMP_CUSTOM_DBKP=${TMP_CUSTOM_DBKP:-$DBKP}
 fi
 
+
 # Create dirs, copy dummy vars file and set bkp dir
 DIRS=(${TMP_CUSTOM_DBKP}/plex-configs,${TMP_CUSTOM_DBKP}/transmission-configs)
 mkdir -p -- "${DIRS[@]}"
 \cp -n "${SCRIPTPATH}/src/dummy-jail.vars" "${TMP_CUSTOM_DBKP}/jail.vars"
-sed -i "" "s/CUSTOM_BKP_DIR=.*/CUSTOM_BKP_DIR=\"${TMP_CUSTOM_DBKP}\"/" "${TMP_CUSTOM_DBKP}/jail.vars"
+sed -i "" "s|CUSTOM_BKP_DIR=.*|CUSTOM_BKP_DIR=\"${TMP_CUSTOM_DBKP}\"|" "${TMP_CUSTOM_DBKP}/jail.vars"
 
 
 exit 1;
