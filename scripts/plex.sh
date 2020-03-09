439a0f68e62bac8fae27fc61906309b06ef81072
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-09 16:12:51 +0200

    z

--- scripts/plex.sh
+++ scripts/plex.sh
@@ -7,11 +7,13 @@ iocage fstab -a ${CUSTOM_JAIL_NAME} "${DPLEXDATA}" "/mnt/plexdata" nullfs rw 0 0
 iocage fstab -a ${CUSTOM_JAIL_NAME} "${DPLEXCONFIG}" "/mnt/plex-config" nullfs rw 0 0
 
 if [ $CUSTOM_USE_PLEXPASS == "yes" ]; then
+  echo ""
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_plexpass_enable='YES'"
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_plexpass_support_path='/mnt/plex-config'"
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_plexpass_user='media'"
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_plexpass_group='ftp'"
 else
+  echo ""
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_enable='YES'"
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_support_path='/mnt/plex-config'"
   iocage exec ${CUSTOM_JAIL_NAME} "sysrc plexmediaserver_user='media'"
@@ -24,7 +26,7 @@ iocage exec ${CUSTOM_JAIL_NAME} "crontab /mnt/plex-config/update_packages"
 # iocage exec ${CUSTOM_JAIL_NAME} "service plexmediaserver start"
 
 # Install WebTools.bundle
-iocage exec ${CUSTOM_JAIL_NAME} wget -O "${DPLEXDATA}/Plex\ Media\ Server/Plug-ins https://github.com/ukdtom/WebTools.bundle/releases/download/3.0.0/WebTools.bundle.zip"
+iocage exec ${CUSTOM_JAIL_NAME} wget "https://github.com/ukdtom/WebTools.bundle/releases/download/3.0.0/WebTools.bundle.zip -P ${DPLEXDATA}/Plex\ Media\ Server/Plug-ins"
 iocage exec ${CUSTOM_JAIL_NAME} unzip "${DPLEXDATA}/Plex\ Media\ Server/Plug-ins/WebTools.bundle.zip"
 iocage exec ${CUSTOM_JAIL_NAME} rm "${DPLEXDATA}/Plex\ Media\ Server/Plug-ins/WebTools.bundle.zip"
 iocage exec ${CUSTOM_JAIL_NAME} chown -R media:ftp "${DPLEXDATA}/Plex\ Media\ Server/Plug-ins/WebTools.bundle"
