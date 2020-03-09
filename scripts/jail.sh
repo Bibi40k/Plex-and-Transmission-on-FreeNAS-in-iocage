638d6598c4ca0da79ef37c75a73c4680c011a337
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-09 12:15:40 +0200

    z

--- scripts/jail.sh
+++ scripts/jail.sh
@@ -23,7 +23,7 @@ iocage create \
     -p "${FPKG}";
 echo ""
 
-MEDIA_GROUP=$(id -G media) # should be 8675309
+MEDIA_GROUP=$(id -g media) # media user group id on FreeNAS (should be 8675309)
 
 iocage exec "${CUSTOM_JAIL_NAME}" env ASSUME_ALWAYS_YES=YES pkg bootstrap
 iocage exec "${CUSTOM_JAIL_NAME}" mkdir -p /mnt/DOWNLOADS
