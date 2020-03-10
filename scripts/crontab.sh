447c44cee1ec3cd62357499b4f008304083355d8
Dan Serban <dan.serban@ejobsgroup.ro>
2020-03-10 11:02:23 +0200

    z

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #write out current crontab
-iocage exec ${CUSTOM_JAIL_NAME} "crontab -l > mycron"
+iocage exec ${CUSTOM_JAIL_NAME} "crontab -l > /tmp/mycron"
 if [ $CUSTOM_USE_PLEXPASS == "yes" ]; then
-    iocage exec ${CUSTOM_JAIL_NAME} echo "18 3 * * 0 pkg upgrade -y && service plexmediaserver-plexpass restart" >> mycron
+    iocage exec ${CUSTOM_JAIL_NAME} echo "18 3 * * 0 pkg upgrade -y && service plexmediaserver-plexpass restart" >> /tmp/mycron
 else
-    iocage exec ${CUSTOM_JAIL_NAME} echo "18 3 * * 0 pkg upgrade -y && service plexmediaserver restart" >> mycron
+    iocage exec ${CUSTOM_JAIL_NAME} echo "18 3 * * 0 pkg upgrade -y && service plexmediaserver restart" >> /tmp/mycron
 fi
 
 while read LINE || [[ -n "$LINE" ]]; do
@@ -13,7 +13,7 @@ while read LINE || [[ -n "$LINE" ]]; do
     iocage exec ${CUSTOM_JAIL_NAME} crontab -l 2>/dev/null | sed -e "\?^plexmediaserver\$?,/^\$/ d" | crontab -
     # We add cronjob
     iocage exec ${CUSTOM_JAIL_NAME} crontab -l | grep "$LINE" || (crontab -l 2>/dev/null; echo "$LINE") | crontab -
-done < mycron
+done < /tmp/mycron
 
-iocage exec ${CUSTOM_JAIL_NAME} echo "mycron is " mycron
-iocage exec ${CUSTOM_JAIL_NAME} rm mycron
+iocage exec ${CUSTOM_JAIL_NAME} echo "mycron is " /tmp/mycron
+# iocage exec ${CUSTOM_JAIL_NAME} rm mycron
