a37bdf3b5d05fe6a36b5e9c6538d12262fc9ac80
Dan Serban <dan.serban@ejobsgroup.ro>
2020-05-12 00:12:59 +0300

    z

--- scripts/_static/autodiscover.sh
+++ scripts/_static/autodiscover.sh
@@ -10,4 +10,7 @@ AUTO_JAIL_IP=$(awk -F"." '{print $1"."$2"."$3".'${SUGGESTED_LAST_OCTET_OF_IP}'"}
 AUTO_RELEASE=$(freebsd-version | sed "s/STABLE/RELEASE/g" | sed "s/-p.*//g")
 AUTO_EXT_IP=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
 AUTO_IP_RANGE=$(netstat -rn | grep -E "U[^A-Z]" | grep -v lo0 | awk '{print $1;}' | sed "s/\/.*//g") # 192.168.1.0
+ZPOOL_NAME=$(iocage get -p)
+ZPOOL_PATH="/mnt/${ZPOOL_NAME}/"
+# ZPOOL_NAME=$(mount | grep /mnt/*/iocage/jails | head -n 1 | sed -e 's/.*\/mnt\/\(.*\)\/iocage\/jails.*/\1/')
 
---
+++ scripts/dirs_for_jail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script will create/copy all necessary files for jailbox
+# Destination folder is '/mnt/ZFS_NAME/iocage/jails/${JAIL_NAME}/root/...'
+
+
+
+# Get iocage path (accessible from FreeNAS)
+IOCAGE_PATH=$(zfs list -o name,mountpoint | grep -m1 iocage/jails/${JAIL_NAME} | awk '{print $2;}')
+
+JAIL_DCONFIG="/root/mediabox-configs" # Custom configs dir inside jail
+JAIL_DBACKUP="/root/backup" # Custom backup dir inside jail
+JAIL_DSERVER="${JAIL_DCONFIG}/server" # Server configs dir inside jail
+JAIL_DLOGS="${JAIL_DCONFIG}/logs" # Logs dir inside jail
+
